3c35c4f3cd605be1d3b5c9fa317be08f855bce23
Techwizz-somboo <44146804+Techwizz-somboo@users.noreply.github.com>
2021-11-13 22:33:24 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 cp change-slot /usr/bin/
+sudo mkdir /usr/share/icons/switch-my-slot/
+cp switch-my-slot.png /usr/share/icons/switch-my-slot/
 cp change-slot.desktop /usr/share/applications/
